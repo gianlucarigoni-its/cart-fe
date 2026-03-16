55f7f0e1210c2acffa40ea61fe6e787155efa62c
Enrico Battistella <enrico.battistella@digitalrockers.it>
2026-03-16 16:57:04 +0100

    generate suffix

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,11 +3,13 @@ import { cart } from './cart-data';
 import { calcCartItem } from './cart-utils';
 import { CartItem } from './cart-item.entity';
 import { FormsModule } from '@angular/forms';
+import { CartItemComponent } from './components/cart-item/cart-item.component';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
-  imports: [FormsModule]
+  imports: [FormsModule, CartItemComponent]
 })
 export class App {
   items = cart;
---
+++ src/app/components/cart-item/cart-item.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-item',
+  imports: [],
+  templateUrl: './cart-item.component.html',
+  styleUrl: './cart-item.component.css',
+})
+export class CartItemComponent {}
